a61d732b8f836079a81e0ecdc2fc79fb66fa9421
Lenasim <radical.rin@gmail.com>
2020-06-01 00:02:03 +0200

    turn no link projet unclickable

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,11 +3,16 @@ import React from 'react';
 export default function Project(props) {
     return (
         <div className="projects_item">
-            <a href={props.link}>
-                <div className="img_container">
+            {props.link ?
+                <a href={props.link}>
+                    <div className="img_container">
+                        <img className="projects_image" src={props.img} alt="" />
+                    </div>
+                </a>
+                : <div className="img_container">
                     <img className="projects_image" src={props.img} alt="" />
                 </div>
-            </a>
+            }
             <div className="projects_text">
                 <a href={props.link}>
                     <h4>{props.name}</h4>
@@ -16,9 +21,9 @@ export default function Project(props) {
                 <p>{props.occasion}</p>
             </div>
             {props.github &&
-                    <a href={props.github}>
-                        <i className="fab fa-github link_git"></i>
-                    </a>
+                <a href={props.github}>
+                    <i className="fab fa-github link_git"></i>
+                </a>
             }
         </div>
     )
